4e658b677a24101d0e6d39432c365f6519fd6e57
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2023-04-16 22:18:44 +0530

    added

--- profile.php
+++ profile.php
@@ -13,54 +13,58 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <h1 class="mt-5">Update Your Profile</h1>
-                <?php
-                if (isset($userProfile)) {
-                    echo $userProfile;
-                }
-                ?>
-                <br />
-            </div>
-
-
+        <div class="row" style="padding:7%">
             <div class="col-lg-12">
-
-                <form action="" method="post">
-                    <?php
-                    $getData = $user->getUserProfile($userId);
-                    if ($getData) {
-                        while ($result = $getData->fetch_assoc()) {
-                            ?>
-                            <table>
-                                <tr>
-                                    <td>Name</td>
-                                    <td><input type="text" name="name" value="<?php echo $result['name']; ?>" id="name"></td>
-                                </tr>
-                                <tr>
-                                    <td>User Name </td>
-                                    <td><input type="text" name="userName" value="<?php echo $result['userName']; ?>"
-                                            id="userName">
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Email</td>
-                                    <td><input name="email" type="email" value="<?php echo $result['email']; ?>" id="email">
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td></td>
-                                    <td><input type="submit" id="profileUpdate" class="btn btn-primary" value="Update">
-                                    </td>
-                                </tr>
-                            </table>
-                        <?php }
-                    } ?>
-                </form>
-                <br />
-                <br />
+                <div class="card" style="width:100%; padding: 0 5%">
+                    <center>
+                        <h5 class="mt-5">Update Your Profile</h5>
+                        <?php
+                        if (isset($userProfile)) {
+                            echo $userProfile;
+                        }
+                        ?>
+                    </center>
+                    <br />
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <?php
+                            $getData = $user->getUserProfile($userId);
+                            if ($getData) {
+                                while ($result = $getData->fetch_assoc()) {
+                                    ?>
+                                    <table>
+                                        <tr>
+                                            Name
+                                            <input type="text" class="form-control" name="name"
+                                                value="<?php echo $result['name']; ?>" id="name">
+                                        </tr>
+                                        <div class="row" style="margin-top:1%">
+                                            <div class="col-sm-6">
+                                                Username
+                                                <input type="text" class="form-control" name="userName"
+                                                    value="<?php echo $result['userName']; ?>" id="userName">
+                                            </div>
+                                            <div class="col-sm-6">
+                                                Email
+                                                <input name="email" class="form-control" type="email"
+                                                    value="<?php echo $result['email']; ?>" id="email">
+                                            </div>
+                                        </div>
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <center>
+                                            <input type="submit" id="profileUpdate" class="btn btn-outline-primary"
+                                                style="border-radius:20px; width:20%" value="Update">
+                                            <center>
+                                    </table>
+                                <?php }
+                            } ?>
+                        </form>
+                        <br />
+                        <br />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
